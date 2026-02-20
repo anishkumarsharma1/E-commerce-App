6e1841393817b37159b0279da6d93261007a7926
anishkumarsharma1 <anishkumarsharma2323@gmail.com>
2026-02-20 12:42:32 +0530

    feat: implement add to cart functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,11 +12,12 @@ import Contact from './pages/Contact'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] ms:px-[7vw] lg:px-[9vw]'>
-        
+      <ToastContainer/>  
         <Navbar/>
         <SearchBar/>
         <Routes>
