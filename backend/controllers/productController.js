5f803b7eb67b6f3d458135100e519cebd3ea4d66
anishkumarsharma1 <anishkumarsharma2323@gmail.com>
2026-02-28 12:58:00 +0530

    feat(product): add create, delete, list, single product APIs and admin auth middleware

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -44,12 +44,39 @@ const addProduct = async (req, res) => {
 };
 
 // function for list all products
-const listProducts = async (req, res) => {};
+const listProducts = async (req, res) => {
+
+    try {
+        const products = await productModel.find({})
+        res.json({success:true,products})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+
+};
 
 // function for removing a product
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true,message:"Product removed successfully"})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+};
 
 // function for single product details
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+    try {
+        const {productId} = req.body
+        const product = await productModel.findById(productId)
+        res.json({success:true,product})
+    } catch (error) {
+        console.log(error)  
+        res.json({success:false,message:error.message})
+    }
+};
 
 export { addProduct, listProducts, removeProduct, singleProduct };
